a311056a4a3d240e4ac322000687dd36f8c783fa
Robert Anderson <kc7rwx@gmail.com>
2011-05-19 23:17:09 -0700

    better organizing the code

--- cicada.rb
+++ cicada.rb
@@ -1,50 +1,56 @@
 require 'RMagick'
+require 'colorize'
 include Magick
 
 class Tile < Image
-  def draw_circle(radius,pos_x,pos_y)
-    #position of circle
-    c_pos_x = rand(self.columns-(radius*2+10))+radius+5
-    c_pos_y = rand(self.rows-(radius*2+10))+radius+5
-    #size of circle
-    c_size_pos_x = c_pos_x + c_size
-    c_size_pos_y = c_pos_y + c_size
-  end
 
-  
-  def rand_size
-    self.resize(rand(1000),rand(1000))
+  def draw_circle(radius,pos_x,pos_y)
+
+    #position of circle 
+    #nudges position if it will be off of the frame
+    border = 1
+
+    if pos_x <= radius
+      pos_x = radius+border
+    elsif pos_x+radius >= self.columns
+      pos_x = self.columns-radius-border
+    else
+    end
+
+    if pos_y <= radius
+      pos_y = radius+border
+    elsif pos_y+radius >= self.rows
+      pos_y = self.rows-radius-border
+    else
+    end 
+
+    #size of circle
+    c_size_pos_x = pos_x - radius
+    
+    @draw.circle(pos_x, pos_y, c_size_pos_x, pos_y)
+    @draw.draw(self)
+    puts @draw.inspect
   end
 
   def draw_star
-    d = Magick::Draw.new
-    500.times do
-      d.fill_opacity(rand).fill('white')
-      d.point(rand(self.columns),rand(self.rows))
+    @draw = Magick::Draw.new
+    rand(100).times do
+      @draw.opacity(rand).fill('white')
+      draw_circle(rand(3),rand(self.columns),rand(self.rows))
     end
-    d.draw(self)
-    puts d.inspect
   end
 
   def rand_circle(size)
     #randomize color
     color_string = 'rgb('+rand(255).to_s+','+rand(255).to_s+','+rand(255).to_s+')'
-    #randomize circle size
-    c_size = rand(size)
-    #position of circle
-    c_pos_x = rand(self.columns-(c_size*2+10))+c_size+5
-    c_pos_y = rand(self.rows-(c_size*2+10))+c_size+5
-    #size of circle
-    c_size_pos_x = c_pos_x + c_size
-    c_size_pos_y = c_pos_y + c_size
 
-    d = Magick::Draw.new
+    @draw = Magick::Draw.new
     
     # Draw the circle
-    d.fill_opacity(0)
-    d.stroke(color_string).stroke_width(1)
-    d.circle(c_pos_x, c_pos_y, c_size_pos_x, c_size_pos_y)
-    d.draw(self)
+    @draw.fill_opacity(0)
+    @draw.stroke(color_string).stroke_width(1)
+    draw_circle(rand(size),rand(self.columns),rand(self.rows))
+    @draw.draw(self)
   end
 
 end
@@ -53,10 +59,11 @@ def rand1()
   i=1
   11.times do
     image = Tile.new(rand(200*i)+250,rand(200*i)+250) {self.background_color='none'}
-    100.times do
-      image.rand_circle(25)
+    15.times do
+      image.rand_circle(50)
     end
     image.write('images/'+i.to_s+'.png')
+    puts 'image '+ i.to_s + ' written'
     i += 1
   end
 end
@@ -72,3 +79,5 @@ def starfield()
     i += 1
   end
 end
+
+starfield
