9ab19205ff5e3c17317d8e9bcbbf0261e9648621
Robert Anderson <kc7rwx@gmail.com>
2011-05-24 08:01:17 -0700

    reorganizing

--- cicada.rb
+++ cicada.rb
@@ -1,79 +1,79 @@
 require 'RMagick'
-require 'colorize'
 include Magick
 
 class Tile < Image
 
+  #@draw = Magick::Draw.new
+
   def draw_circle(radius,pos_x,pos_y)
 
-    #position of circle 
+    #position of circle
     #nudges position if it will be off of the frame
     border = 2
-
     if pos_x <= radius
       pos_x = radius+border
     elsif pos_x+radius >= self.columns
       pos_x = self.columns-radius-border
     else
     end
-
     if pos_y <= radius
       pos_y = radius+border
     elsif pos_y+radius >= self.rows
       pos_y = self.rows-radius-border
     else
-    end 
+    end
+
     @draw.circle(pos_x, pos_y, pos_x-radius , pos_y)
-    puts @draw.inspect
   end
 
-  def draw_star
-    @draw = Magick::Draw.new
-    rand(100).times do
-      @draw.opacity(rand).fill('white')
-      draw_circle(rand(3),rand(self.columns),rand(self.rows))
-    end
-    @draw.draw(self)
+  def draw_rectangle(center,size_x,size_y)
+    point_1_x = center-x_size
+    point_1_y = center-y_size
+    point_2_x = center+x_size
+    point_2_y = center+y_size
+    @draw.rectangle(point_1_x,point_1_y, point_2_x,point_2_y)
   end
 
-  def rand_circle(size)
-    #randomize color
-    color_string = 'rgb('+rand(255).to_s+','+rand(255).to_s+','+rand(255).to_s+')'
-
-    @draw = Magick::Draw.new
-    
-    # Draw the circle
-    @draw.fill_opacity(0)
-    @draw.stroke(color_string).stroke_width(1)
-    15.times do
-      draw_circle(rand(size),rand(self.columns),rand(self.rows))
-    end
+  def draw_to_tile
     @draw.draw(self)
   end
 
 end
 
-def rand1()
-  i=1
-  11.times do
-    image = Tile.new(rand(200*i)+250,rand(200*i)+250) {self.background_color='none'}
-    image.rand_circle(50)
-    image.write('images/'+i.to_s+'.png')
-    puts 'image '+ i.to_s + ' written'
+
+class TileSet
+
+  def initialize
+    @images = []
+  end
+
+  def create_random(seed_size,num_tiles)
+    i=1
+    num_tiles.times do
+      @images[i] = Tile.new(rand(seed_size*i),rand(seed_size*i)) {self.background_color='none'}
+      i += 1
+    end
+  end
+
+  def create_grid(grid_size,num_tiles)
+  end
+
+  def write_tile_set
+    i=0
+    @images.each do |image|
+      image.write('images/'+i.to_s+'.png')
     i += 1
+    end
+  end
+
+end
+
+def generate_rand1
+  t = TileSet.new
+  t.create_random(50,10)
+  t.each do |tile|
+    tile.draw_circle(rand(10),rand(tile.columns),rand(tile.rows))
+    tile.draw_to_tile
   end
 end
 
-
-
-def starfield()
-  i=1
-  11.times do
-    image = Tile.new(rand(200*i)+250,rand(200*i)+250) {self.background_color='none'}
-    image.draw_star()
-    image.write('images/'+i.to_s+'.png')
-    i += 1
-  end
-end
-
-starfield
